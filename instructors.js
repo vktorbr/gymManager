c2f503ce4ded8862686f54a7a0750e3169d67b3e
Victor Martins <vms5@cin.ufpe.br>
2021-10-08 01:13:01 -0300

    criacao das paginas de visualizacao e edicao do instrutor

--- instructors.js
+++ instructors.js
@@ -1,5 +1,28 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { age, date } = require('./utils');
+
+//show
+exports.show = function(req, res){
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found.");
+
+    
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
+    }
+
+    return res.render('instructors/show', { instructor });
+}
 
 //create
 exports.post = function(req, res){
@@ -35,4 +58,23 @@ exports.post = function(req, res){
     })
 
     //return res.send(req.body);
+}
+
+//edit
+exports.edit = function(req, res){
+
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id;
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found!");
+    
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor });
 }
---
+++ utils.js
@@ -0,0 +1,25 @@
+module.exports = {
+    age: function(timestamp){
+        const today = new Date();
+        const birthDate = new Date(timestamp);
+    
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const month = today.getMonth() - birthDate.getMonth();
+        const day = today.getDate() - birthDate.getDate();
+    
+        if(month < 0 || month == 0 && day < 0){
+            age--;
+        }
+    
+        return age;
+    },
+    date: function(timestamp){
+        const date = new Date(timestamp);
+
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        return `${year}-${month}-${day}`;
+    }
+}
