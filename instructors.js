0e4202ceb78c6b86ce06fffa63633960b90598a6
Victor Martins <vms5@cin.ufpe.br>
2021-10-10 21:20:26 -0300

    criacao da página de listagens de instrutores

--- instructors.js
+++ instructors.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const data = require('./data.json');
 const { age, date } = require('./utils');
 
+//index
+exports.index = function(req, res){
+    return res.render("instructors/index", { instructors: data.instructors });
+}
+
 //show
 exports.show = function(req, res){
     const { id } = req.params;
@@ -96,7 +101,8 @@ exports.put = function(req, res){
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor;
