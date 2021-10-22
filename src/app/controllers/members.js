b523b7052189fa4b1aaae58384fcf6b265397d7f
Victor Martins <vms5@cin.ufpe.br>
2021-10-22 14:15:21 -0300

    Refatoracao do CRUD dos instrutores e estudantes com o banco de dados

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,8 +1,12 @@
+const Member = require('../models/member');
 const { age, date } = require('../../lib/utils');
 
 module.exports = {
     index(req, res){
-        return res.render("members/index", { members: data.members });
+        Member.all(function(members){
+            return res.render("members/index", { members });
+        })
+
     },
     create(req, res){
         return res.render('members/create');
@@ -16,15 +20,28 @@ module.exports = {
             }
         }
 
-        let { birth, services, name, gender, avatar_url } = req.body;
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`);
+        })
 
-        return;
     },
     show(req, res){
-        return;
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Member not found!");
+
+            member.birth = date(member.birth).birthDay;
+
+            return res.render("members/show", {member});
+        })
     },
     edit(req, res){
-        return;
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Member not found!");
+
+            member.birth = date(member.birth).iso;
+
+            return res.render("members/edit", {member});
+        })
     },
     put(req, res){
         const keys = Object.keys(req.body);
@@ -35,9 +52,13 @@ module.exports = {
             }
         }
 
-        return;
+        Member.update(req.body, function(){
+            return res.redirect(`/members/${req.body.id}`);
+        })
     },
     delete(req, res){
-
+        Member.delete(req.body.id, function(){
+            return res.redirect("/members");
+        })
     },
 }
